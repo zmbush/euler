6012f7e8730bb73deec67c61e6fa2c28bc3fc67f
Zachary Bush <zachary.bush@meraki.net>
2015-02-06 10:51:16 -0800

    Add solution to problem 4

---
+++ 0004/src/main.rs
@@ -0,0 +1,47 @@
+#[macro_use] extern crate libeuler;
+use std::num::Int;
+
+/// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+///
+/// Find the largest palindrome made from the product of two 3-digit numbers.
+fn main() {
+    solutions!{
+        inputs: (digits: i64 = 3)
+
+        sol naive {
+            let min = 10.pow((digits - 1) as usize); // 1 digit = 10^0, 2 digit = 10^1, 3 digit = 10^2
+            let max = 10.pow(digits as usize);
+            let mut max_palindrome: i64 = 0;
+
+            for first in min..max {
+                for second in first..max {
+                    let prod = first*second;
+                    if prod > max_palindrome && prod.is_palindrome() {
+                        max_palindrome = prod;
+                    }
+                }
+            }
+
+            max_palindrome
+        }
+    };
+}
+
+trait PalindromeHelper {
+    fn is_palindrome(&self) -> bool;
+}
+
+impl PalindromeHelper for String {
+    fn is_palindrome(&self) -> bool {
+        let forward = self.chars().take(self.len() / 2);
+        let reverse = self.chars().rev().take(self.len() / 2);
+        let both_directions = forward.zip(reverse);
+        both_directions.all(|(a, b)| { a == b })
+    }
+}
+
+impl PalindromeHelper for i64 {
+    fn is_palindrome(&self) -> bool {
+        format!("{}", self).is_palindrome()
+    }
+}
