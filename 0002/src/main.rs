d3be36827b40c51f7dcda1f63835f6894fad1b27
Zachary Bush <zachary.bush@meraki.net>
2015-02-05 20:06:38 -0800

    Add solution to problem 2

---
+++ 0002/src/main.rs
@@ -0,0 +1,25 @@
+#[macro_use] extern crate libeuler;
+
+fn main() {
+    solutions!{
+        inputs: (max_value: i64 = 4_000_000)
+
+        sol naive {
+            let mut prev = 0;
+            let mut value = 1;
+            let mut result = 0;
+
+            while value < max_value {
+                let tmp = prev+value;
+                prev = value;
+                value = tmp;
+
+                if (value % 2 == 0) {
+                    result += value;
+                }
+            }
+
+            result
+        }
+    };
+}
