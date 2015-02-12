4740622a6e36a69d3ed0c32157dae713e1356143
Zachary Bush <zachary.bush@meraki.net>
2015-02-11 23:09:54 -0800

    Add solution to problem 11

---
+++ sol_0011/src/main.rs
@@ -0,0 +1,175 @@
+#[macro_use] extern crate libeuler;
+use std::num::Int;
+use std::iter::AdditiveIterator;
+use std::iter::MultiplicativeIterator;
+use libeuler::SieveOfAtkinIterator;
+
+
+
+
+/// In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+///
+///            08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+///            49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+///            81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+///            52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+///            22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+///            24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+///            32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+///            67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+///            24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+///            21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+///            78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+///            16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+///            86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+///            19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+///            04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+///            88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+///            04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+///            20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+///            20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+///            01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+///
+/// The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+///
+/// What is the greatest product of four adjacent numbers in the same direction (up, down, left,
+/// right, or diagonally) in the 20×20 grid?
+fn main() {
+    let grid = [
+        [ 08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08 ],
+        [ 49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00 ],
+        [ 81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65 ],
+        [ 52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91 ],
+        [ 22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80 ],
+        [ 24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50 ],
+        [ 32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70 ],
+        [ 67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21 ],
+        [ 24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72 ],
+        [ 21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95 ],
+        [ 78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92 ],
+        [ 16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57 ],
+        [ 86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58 ],
+        [ 19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40 ],
+        [ 04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66 ],
+        [ 88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69 ],
+        [ 04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36 ],
+        [ 20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16 ],
+        [ 20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54 ],
+        [ 01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48 ],
+    ];
+
+    enum GridIterDirection {
+        Right,
+        Down,
+        DiagonalRight,
+        DiagonalLeft
+    }
+
+
+    struct GridIter {
+        direction: Option<GridIterDirection>,
+        x: usize,
+        y: usize,
+        grid: [[i32; 20]; 20]
+    }
+
+    impl GridIter {
+        fn new(grid: [[i32; 20]; 20]) -> GridIter {
+            GridIter {
+                direction: Some(GridIterDirection::Right),
+                x: 0,
+                y: 0,
+                grid: grid
+            }
+        }
+    }
+    impl Iterator for GridIter {
+        type Item = Vec<i32>;
+
+        fn next(&mut self) -> Option<Vec<i32>> {
+            if self.direction.is_none() {
+                return None;
+            }
+
+            println!("({}, {}) {}", self.x, self.y, match self.direction {
+                Some(GridIterDirection::Right) => "Right",
+                Some(GridIterDirection::DiagonalRight) => "DiagonalRight",
+                Some(GridIterDirection::DiagonalLeft) => "DiagonalLeft",
+                Some(GridIterDirection::Down) => "Down",
+                None => unreachable!()
+            });
+
+            let mut retval = Vec::new();
+            for i in 0..4 {
+                retval.push(match self.direction {
+                    Some(GridIterDirection::Right) => self.grid[self.y][self.x + i],
+                    Some(GridIterDirection::DiagonalRight) => self.grid[self.y + i][self.x + i],
+                    Some(GridIterDirection::DiagonalLeft) => self.grid[self.y + i][self.x - i],
+                    Some(GridIterDirection::Down) => self.grid[self.y + i][self.x],
+                    None => unreachable!()
+                });
+            }
+
+            loop {
+                self.direction = match self.direction {
+                    Some(GridIterDirection::Right) => Some(GridIterDirection::DiagonalLeft),
+                    Some(GridIterDirection::DiagonalLeft) => Some(GridIterDirection::DiagonalRight),
+                    Some(GridIterDirection::DiagonalRight) => Some(GridIterDirection::Down),
+                    Some(GridIterDirection::Down) => {
+                        self.x += 1;
+                        if self.x >= self.grid[0].len() {
+                            self.x = 0;
+                            self.y += 1;
+                            if self.y >= self.grid.len() {
+                                None
+                            } else {
+                                Some(GridIterDirection::Right)
+                            }
+                        } else {
+                            Some(GridIterDirection::Right)
+                        }
+                    },
+                    None => unreachable!()
+                };
+
+                match self.direction {
+                    Some(GridIterDirection::Right) => {
+                        if self.x + 3 < self.grid[0].len() {
+                            break;
+                        }
+                    },
+                    Some(GridIterDirection::DiagonalRight) => {
+                        if self.x + 3 < self.grid[0].len() && self.y + 3 < self.grid.len() {
+                            break;
+                        }
+                    },
+                    Some(GridIterDirection::DiagonalLeft) => {
+                        if (self.x as isize)- 3 >= 0 && self.y + 3 < self.grid.len() {
+                            break;
+                        }
+                    },
+                    Some(GridIterDirection::Down) => {
+                        if self.y + 3 < self.grid.len() {
+                            break;
+                        }
+                    },
+                    None => break
+                }
+            }
+
+            return Some(retval);
+        }
+    }
+
+    solutions!{
+        inputs: (ceiling: u64 = 2_000_000)
+
+        sol naive {
+            GridIter::new(grid).map(|v| {
+                let ret = v.iter().map(|&x| x).product();
+                println!("getting product of {:?} -> {}", v, ret);
+                ret
+            }).max()
+        }
+    };
+}
